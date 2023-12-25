2ec7210fad2a4bb1ffaaf447a5f7259fdad51f52
Storm <stormpower44@protonmail.com>
2023-12-24 21:27:45 -0700

    WORKS

--- plant-waterer.ino
+++ plant-waterer.ino
@@ -3,7 +3,7 @@ int water_delay =  15 * 1000; // in seconds | * 60000; // in hours
 
 int reset_button_pin = 2;
 int water_button_pin = 3;
-int water_pump_pin = 4;
+int water_pump_control = 9;
 
 unsigned long time;
 unsigned long start_time = 0;
@@ -11,17 +11,17 @@ unsigned long start_time = 0;
 void setup() {
   pinMode(reset_button_pin, INPUT);
   pinMode(water_button_pin, INPUT);
-  pinMode(water_pump_pin, OUTPUT);
+  pinMode(water_pump_control, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   time = millis();
   if (time >= water_delay + start_time) {
-    digitalWrite(water_pump_pin, HIGH);
+    digitalWrite(water_pump_control, HIGH);
     if (time >= water_delay + start_time + water_time) {
       start_time = time;
-      digitalWrite(water_pump_pin, LOW);
+      digitalWrite(water_pump_control, LOW);
     }
   }
  
@@ -29,22 +29,22 @@ void loop() {
 
   if (digitalRead(water_button_pin)) {
     Serial.println("Pumping water (button press).");
-    digitalWrite(water_pump_pin, HIGH);
+    digitalWrite(water_pump_control, HIGH);
     delay(water_time);
-    digitalWrite(water_pump_pin, LOW);
+    digitalWrite(water_pump_control, LOW);
   }
 
   if (digitalRead(reset_button_pin)) {
     Serial.println("Resetting water timer (button press).");
-    digitalWrite(water_pump_pin, LOW);
+    digitalWrite(water_pump_control, LOW);
     start_time = millis();
   }
 
   /* SIMPLE CODE, no input
   delay(water_delay); 
-  digitalWrite(water_pump_pin, HIGH);
+  digitalWrite(water_pump_control, HIGH);
   delay(water_time);
-  digitalWrite(water_pump_pin, LOW);
+  digitalWrite(water_pump_control, LOW);
   */
 }
 
